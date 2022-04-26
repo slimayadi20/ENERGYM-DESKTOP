976e840bb012aa6f1af9c145ac5708e1219de0f8
slimayadi20 <slim.ayadi@esprit.tn>
2022-04-27 01:31:19 +0200

    front

--- src/energym/desktop/MainFX.java
+++ src/energym/desktop/MainFX.java
@@ -30,7 +30,7 @@ public class MainFX extends Application {
     public void start(Stage primaryStage) {
           try {
 
-            Parent root = FXMLLoader.load(getClass().getResource("/GUI/FXML.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/GUI/Front.fxml"));
             //      Parent root = FXMLLoader.load(getClass().getResource("/GUI/UpdatePassword.fxml"));
             //   Parent root = FXMLLoader.load(getClass().getResource("/GUI/User.fxml"));
 
